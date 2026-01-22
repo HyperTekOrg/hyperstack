222c6255a9c492a0dd6ea78c30f5cf0edf780697
Adrian Henry <adrian@hungryturtlecode.com>
2026-01-22 03:39:09 +0000

    fix: Log level in pumpfun server

--- examples/pumpfun-server/src/main.rs
+++ examples/pumpfun-server/src/main.rs
@@ -15,7 +15,12 @@ async fn main() -> anyhow::Result<()> {
         dotenvy::from_path(&env_path)?;
     }
 
-    tracing_subscriber::fmt().with_env_filter("info").init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
 
     let spec = pumpfun_stream::spec();
 
