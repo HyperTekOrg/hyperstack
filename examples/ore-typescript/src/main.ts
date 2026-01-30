464be52a1cccac793b3eb08612e770869c60a974
Adrian Henry <adrian@hungryturtlecode.com>
2026-01-30 18:45:45 +0000

    chore(examples): update ore-typescript to use .use() API

--- examples/ore-typescript/src/main.ts
+++ examples/ore-typescript/src/main.ts
@@ -1,38 +1,26 @@
-import { HyperStack, type Update } from 'hyperstack-typescript';
+import { HyperStack } from 'hyperstack-typescript';
 import { OREROUND_STACK, type OreRound } from 'hyperstack-stacks/ore';
 
 const URL = 'wss://ore.stack.usehyperstack.com';
 
-function printRoundUpdate(prefix: string, key: string, data: OreRound) {
-  console.log(`\n[${prefix}] === Round Update ===`);
-  console.log(`[${prefix}] Key: ${key}`);
-  console.log(`[${prefix}] Round ID: ${data.id?.round_id ?? 'N/A'}`);
-  console.log(`[${prefix}] Address: ${data.id?.round_address ?? 'N/A'}`);
-  console.log(`[${prefix}] Motherlode: ${data.state?.motherlode ?? 'N/A'}`);
-  console.log(`[${prefix}] Total Deployed: ${data.state?.total_deployed ?? 'N/A'}`);
-  console.log(`[${prefix}] Expires At: ${data.state?.expires_at ?? 'N/A'}`);
-  console.log(`[${prefix}] Metrics: ${JSON.stringify(data.metrics)}`);
+function printRound(round: OreRound) {
+  console.log(`\n=== Round #${round.id?.round_id ?? 'N/A'} ===`);
+  console.log(`Address: ${round.id?.round_address ?? 'N/A'}`);
+  console.log(`Motherlode: ${round.state?.motherlode ?? 'N/A'}`);
+  console.log(`Total Deployed: ${round.state?.total_deployed ?? 'N/A'}`);
+  console.log(`Expires At: ${round.state?.expires_at ?? 'N/A'}`);
+  console.log(`Deploy Count: ${round.metrics?.deploy_count ?? 0}`);
   console.log();
-}
-
-function handleUpdate(prefix: string, update: Update<OreRound>) {
-  if (update.type === 'upsert' || update.type === 'patch') {
-    printRoundUpdate(prefix, update.key, update.data as OreRound);
-  } else if (update.type === 'delete') {
-    console.log(`[${prefix}] Round deleted: ${update.key}\n`);
-  }
 }
 
 async function main() {
   const hs = await HyperStack.connect(URL, {
     stack: OREROUND_STACK,
   });
-  console.log(`Connected to ${URL}`);
+  console.log(`Connected to ${URL}\n`);
 
-  console.log('=== Watching Latest view ===');
-
-  for await (const update of hs.views.OreRound.latest.watch()) {
-    handleUpdate('LATEST', update);
+  for await (const round of hs.views.OreRound.latest.use()) {
+    printRound(round);
   }
 }
 
